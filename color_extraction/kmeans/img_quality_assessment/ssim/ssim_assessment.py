1fd419ca28ff5133fc3481576bfc1fdbe337c67e
Sunny <wnn2260@gmail.com>
2018-07-13 16:49:27 -0400

    assessment update

--- color_extraction/kmeans/img_quality_assessment/ssim/ssim_assessment.py
+++ color_extraction/kmeans/img_quality_assessment/ssim/ssim_assessment.py
@@ -7,15 +7,27 @@ import  matplotlib.colors as mcolors
 from PIL import Image
 import ssim
 import glob
+import csv
 
-temp_img = Image.open('../img/sky.jpg')
+
+
+temp_img = Image.open('../../../img/sky.jpg')
 
 quantized_img_path_list = []
-quantized_img_path_list = glob.glob(r'img/sky/quantized_img/*.png')
+quantized_img_path_list = glob.glob(r'../../img/sky/quantized_img/*.png')
 quantized_img_path_list.sort()
 print(quantized_img_path_list)
 
+score_list = []
 for i in quantized_img_path_list:
     img = Image.open(i)
     score = ssim.compute_ssim(img,temp_img)
+    score_list.append(score)
     print(score)
+
+csvfile = "sky_ssim.csv"
+with open(csvfile, "w") as output:
+    writer = csv.writer(output, lineterminator='\n')
+    for val in score_list:
+        writer.writerow([val])
+
